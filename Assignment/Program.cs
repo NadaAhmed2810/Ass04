feb88ac0036a65af4e5faeb8617190454ea54a45
NadaAhmed2810 <nwrabsywny534@gmail.com>
2024-12-24 23:44:05 +0200

    Q4

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assignment
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment
 {
     internal class Program
     {
@@ -12,16 +14,53 @@
             num += 10;
             Console.WriteLine(num);
         }
+        public static void UpdateNameOfStudent(Student std,string s)
+        {
+            std.Name = s;
+        }
+        public static void UpdateNameOfStudent(ref Student std, string s)
+        {
+            std.Name = s;
+        }
+        public static void updateStudent(Student std, string s)
+        {
+            std=new Student();
+            std.Name=s;
+            std.Id = 10;
+        }
+        public static void updateStudent(ref Student std, string s)
+        {
+            std = new Student();
+            std.Name = s;
+            std.Id = 10;
+        }
+        public static void CalcSumAndSub(int x,int y,out int sum,out int sub)
+        {
+            sum = x + y;
+            sub = x - y;
+        }
+        public static int CalcSumOfdigits(int x)
+        {
+            string s = x.ToString();
+            int sum = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                sum += s[i]-'0';
+            }
+            return sum;
+        }
         static void Main(string[] args)
         {
             #region Q1: Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
-            #region Value type
+            #region Value type passing by value
             ////Function By Value works on a copy value and  do not affect the original variable
             //int num = 10;
             //Console.WriteLine("Passing by Value: ");
             //Console.WriteLine($"Before: {num}");
             //IncreaseNumber( num);
             //Console.WriteLine($"After:{num}");
+            #endregion
+            #region Value type passing by reference 
 
             ////works on actual variable and do affect the original variable 
             //Console.WriteLine("Passing by Reference: ");
@@ -30,6 +69,61 @@
             //Console.WriteLine($"After:{num}");
             #endregion
             #endregion
+            #region Q2:Explain the difference between passing (Reference type parameters) by value and by reference then write a suitable c# example.
+            #region reference passing By value
+            #region update value in this object
+            //function to update Name of Student
+            //function take address of object so it work in object if i update value in this object
+            //Student student= new Student();
+            //student.Name = "Nada";
+            //Console.WriteLine(student?.Name??" ");
+            //if(student!=null)
+            //   UpdateNameOfStudent(student, "Nour");
+            //Console.WriteLine(student?.Name??" ");
+            #endregion
+            #region make new object 
+            //function to update Name of Student
+            //function take address of object so it work in object if make new object in function original object not effect
+            //Student student = new Student();
+            //student.Name = "Nada";
+            //Console.WriteLine(student?.Name ?? " ");
+            //if (student != null)
+            //    updateStudent(student, "Nour");
+            //Console.WriteLine(student?.Name ?? " ");
+            #endregion
+            #endregion
+            #region reference type passing By ref 
+            //function take ref of object so work on object always
+            //Student student = new Student();
+            //student.Name = "Noura";
+            //Console.WriteLine(student?.Name ?? " ");
+            //if (student != null)
+            //    UpdateNameOfStudent(ref student, "Nour");
+            //Console.WriteLine(student?.Name ?? " ");
+            #endregion
+            #region make new object 
+            //function to update Student
+            //function take  object so it work in object if make new object in function original object effect
+            //Student student = new Student();
+            //student.Name = "Nada";
+            //Console.WriteLine(student?.Name ?? " ");
+            //if (student != null)
+            //    updateStudent(ref student, "Nour");
+            //Console.WriteLine(student?.Name ?? " ");
+            #endregion
+            #endregion
+            #region Q3:Write a c# Function that accept 4 parameters from user and return result of summation and subtracting of two numbers
+            //int x = 50, y = 10, sum, sub;
+            //CalcSumAndSub(x, y, out sum, out sub);
+            //Console.WriteLine($"Sum: {sum}\nSub: {sub}");
+
+            #endregion
+            #region Q4:Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number.
+            //Console.Write("Enter Number: ");
+            //int Num =int.Parse(Console.ReadLine());
+            //Console.WriteLine(CalcSumOfdigits(Num));
+            #endregion
+
         }
     }
 }
