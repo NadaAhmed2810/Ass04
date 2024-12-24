e5b123906fd437b662a0caa7113cf7960831a91a
NadaAhmed2810 <nwrabsywny534@gmail.com>
2024-12-24 14:46:01 +0200

    Boxing and UnBoxing

--- Ass04/Program.cs
+++ Ass04/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello,World");
+
+            #region Casting [Boxing,unBoxing]
+            // takes time and memory Not recommended 
+            //ref type casting=> ref type to ref type 
+            //Boxing Value type to ref type [Safe Casting]
+            //unBoxing Casting => Ref type to value type [unsafe Casting]
+            //object obj = new object;
+            //obj = "Mostafa";// reference type casting
+            //obj = 1;        //Boxing [Casting from value type to reference type]
+            #region Boxing [Safe Casting]
+            //int x = 10;
+            //object obj01 = new object();
+            //obj01 = x;
+            //Console.WriteLine(obj01);
+            //Parent =child
+            //Dog is an Animal
+            #endregion
+
+            #region UnBoxing[unsafe casting]
+            //object obj02 = new object();
+            //obj02 = "mostafa";
+            //int x = (int)obj02;
+            //Console.WriteLine(x);
+            ////Child =Parent 
+            ////Animals ares Dogs
+            #endregion
+            #endregion
         }
     }
 }
